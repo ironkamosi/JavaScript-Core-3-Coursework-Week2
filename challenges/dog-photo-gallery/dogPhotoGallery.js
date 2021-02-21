d4c5f1af1bb01dab26653a2af826d4bddaa65774
ironkamosi <alintonchambers@gmail.com>
2021-02-21 16:52:45 +0100

    refacted the code called the JSON function

--- challenges/dog-photo-gallery/dogPhotoGallery.js
+++ challenges/dog-photo-gallery/dogPhotoGallery.js
@@ -2,22 +2,29 @@
 function dogGallery() {
   let divContainer = document.getElementById("root");
   let button = document.createElement("button");
+  button.setAttribute("id", "press");
+
+  // button.style.backgroundColor = "green";
+  button.textContent = "press me";
+
   let ulElement = document.createElement("ul");
   let liElement = document.createElement("li");
   let imgElement = document.createElement("img");
 
-  divContainer.appendChild(buttonOne);
+  divContainer.appendChild(button);
   divContainer.appendChild(ulElement);
-
+  
   button.addEventListener("click", function () {
       fetch("https://dog.ceo/api/breeds/image/random")
-          .then((data) => data.json)
+          .then((data) => data.json())
           .then((data) => {
               (imgElement.src = data.message),
                   liElement.appendChild(imgElement),
                   ulElement.appendChild(liElement),
-                  console.log("status of data", data.status);
+                  console.log("status of data", data);
           })
-          .catch(error => console.log(error));  
+          .catch(error => console.log("problem",error));  
   });
 }
+
+dogGallery()
