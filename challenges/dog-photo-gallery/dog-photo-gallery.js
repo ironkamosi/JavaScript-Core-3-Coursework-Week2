7688334ee4880a83e9458491c512226cdccab2e2
ironkamosi <alintonchambers@gmail.com>
2021-02-21 15:57:47 +0100

    add more logic to the add eventListener

--- challenges/dog-photo-gallery/dog-photo-gallery.js
+++ challenges/dog-photo-gallery/dog-photo-gallery.js
@@ -1,22 +1,23 @@
-
-// function to create images 
+// function to create images
 function dogGallery() {
-    let divContainer = document.getElementById("root");
-    let button = document.createElement("button");
-    let ulElement = document.createElement("ul");
-    let liElement = document.createElement("li");
-    let imgElement = document.createElement("img");
+  let divContainer = document.getElementById("root");
+  let button = document.createElement("button");
+  let ulElement = document.createElement("ul");
+  let liElement = document.createElement("li");
+  let imgElement = document.createElement("img");
 
-    divContainer.appendChild(buttonOne);
-    divContainer.appendChild(ulElement);
+  divContainer.appendChild(buttonOne);
+  divContainer.appendChild(ulElement);
 
-    button.addEventListener("click",function() {
-        fetch("https://dog.ceo/api/breeds/image/random")
-            .then(data => data.json).then(
-          
-
-            liElement.appendChild(imgElement),
-            ulElement.appendChild(liElement)
-        )
-    })
+  button.addEventListener("click", function () {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((data) => data.json)
+      .then((data) => {
+        (imgElement.src = data.message),
+          liElement.appendChild(imgElement),
+          ulElement.appendChild(liElement),
+          console.log("status of data", data.status);
+      })
+      .catch(console.log (error=>error))  
+  });
 }
